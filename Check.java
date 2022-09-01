39303d64b43f65dea840efc3a1a8460166efa17d
Awonke11 <mnotozawonke@gmail.com>
2022-09-01 14:09:55 +0200

    Done

---
+++ Check.java
@@ -0,0 +1,11 @@
+public class Check {
+    private double amount;
+
+    Check(double amount) {
+        this.amount = amount;
+    }
+
+    public double getamount() {
+        return this.amount;
+    }
+}
